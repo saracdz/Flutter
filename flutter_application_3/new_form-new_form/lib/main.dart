7d875961785a6560b080ca84cc9cda68d21e889d
sara <sara.conejo@sarria.salesians.cat>
2025-11-22 03:52:55 +0100

    botones

--- flutter_application_3/new_form-new_form/lib/main.dart
+++ flutter_application_3/new_form-new_form/lib/main.dart
@@ -35,44 +35,116 @@ class _MyCustomFormState extends State<MyCustomForm> {
       appBar: AppBar(
         title: const Text('Recuperar el valor d\'un camp de text'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              backgroundColor: Colors.transparent,
-              context: context,
-              builder: (BuildContext context) {
-                return Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
-                      ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+
+          child: Column(
+            children: <Widget>[
+
+              TextField( 
+              controller: myController,
+              ),
+
+              Spacer(),
+
+              OutlinedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                      builder: (BuildContext context) {
+                        return SimpleDialog(
+                          title: const Text('Simple Dialog'),
+                          children: [
+                            SimpleDialogOption(
+                                child: Text(myController.text),
+                            ),
+                          ],
+                        );
+                      },
+                  );
+                },
+                  child: const Text('Simple Dialog'),
+              ),
+
+              const SizedBox(height: 20),
+
+              OutlinedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Alert Dialog'),
+                          content: Text(myController.text),
+                          actions: [  
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('OK'),
+                            )
+                          ],
+                        );
+                      },
+                  );
+                },
+                  child: const Text('Alert Dialog'),
+              ),
+            
+              const SizedBox(height: 20),
+
+              OutlinedButton(
+                onPressed: (){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                      content: Text(myController.text),
+                      duration: Duration(seconds: 2),
                     ),
-                    height: 200,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(myController.text),
-                          ElevatedButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: const Text('Tancar BottomSheet'),
-                          )
-                        ],
-                      ),
-                    ));
-              });
-        },
-        tooltip: 'Mostra el valor!',
-        child: const Icon(Icons.text_fields),
+                  );
+                }, 
+                child: Text('SnackBar'), 
+              ),
+
+              const SizedBox(height: 20),
+
+              OutlinedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                    backgroundColor: Colors.transparent,
+                    context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                            ),
+                          ),
+                          height: 200,
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(myController.text),
+                                ElevatedButton(
+                                  child: const Text('Close'),
+                                  onPressed:() => Navigator.of(context).pop(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                  );
+                },
+                child: const Text('Modal Bottom Sheet'),
+              ),
+
+              Spacer(),
+            ]
+          ),
       ),
     );
   }
